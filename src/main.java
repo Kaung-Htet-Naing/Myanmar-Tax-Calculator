8423e0082fc3abc16121fda5a7a748e44998ec83
Kaung Htet Naing <11kaungkaung99@gmail.com>
2020-04-02 15:12:21 +0630

    Final

--- src/main.java
+++ src/main.java
@@ -13,119 +13,91 @@ public class main {
     private static double totalTax;
     private static double monthlyTax;
 
-    public static void main (String args[]){
 
-        Scanner sc=new Scanner(System.in);
+    public static void main (String args[]) {
+
+        Scanner sc = new Scanner(System.in);
         System.out.print("\u001B[32mEnter Monthly Salary ( MMK ) : ");
-        monthlySalary=sc.nextDouble();
-        annualSalary=12*monthlySalary;
+        monthlySalary = sc.nextDouble();
+        annualSalary = 12 * monthlySalary;
         System.out.print("Enter Number of Months : ");
-        int months=sc.nextInt();
-        personalRelief=(months*monthlySalary)*20/100;
+        int months = sc.nextInt();
+        personalRelief = (months * monthlySalary) * 20 / 100;
 
         sc.nextLine();
-        while(true){
+        while (true) {
             System.out.print("Do You Have A Father ( Yes / No ) ? ");
             String quesFather = sc.nextLine();
-            if (quesFather.equals("Yes") ) {
-                parentRelief+=1000000;
+            if (quesFather.equals("Yes")) {
+                parentRelief += 1000000;
                 break;
-            }
-            else if(quesFather.equals("No")){
+            } else if (quesFather.equals("No")) {
                 break;
-            }
-            else {
+            } else {
                 System.out.println("Invalid Input Pls Try Again ");
             }
         }
 
-        while(true){
+        while (true) {
             System.out.print("Do You Have A Mother ( Yes / No ) ? ");
             String quesMother = sc.nextLine();
             if (quesMother.equals("Yes")) {
-                parentRelief+=1000000;
+                parentRelief += 1000000;
                 break;
-            }
-            else if(quesMother.equals("No")){
+            } else if (quesMother.equals("No")) {
                 break;
-            }else {
+            } else {
                 System.out.println("Invalid Input Pls Try Again ");
             }
         }
 
-        while(true){
+        while (true) {
             System.out.print("Do You Have A Spouse ( Yes / No ) ? ");
             String quesSpouse = sc.nextLine();
             if (quesSpouse.equals("Yes")) {
-                spouseRelief=1000000;
+                spouseRelief = 1000000;
                 break;
-            }
-            else if(quesSpouse.equals("No")) {
+            } else if (quesSpouse.equals("No")) {
                 break;
-            }
-            else {
+            } else {
                 System.out.println("Invalid Input Pls Try Again ");
             }
         }
 
-        while(true){
+        while (true) {
             System.out.print("Do You Have A Children ( Yes / No ) ? ");
             String quesChildren = sc.nextLine();
             if (quesChildren.equals("Yes")) {
                 System.out.print("How Many Children Do You Have ? ");
-                int nunOfChildren=sc.nextInt();
-                childrenRelief=nunOfChildren*500000;
+                int nunOfChildren = sc.nextInt();
+                childrenRelief = nunOfChildren * 500000;
                 sc.nextLine();
                 break;
-            }
-            else if(quesChildren.equals("No")){
+            } else if (quesChildren.equals("No")) {
                 break;
-            }
-            else {
+            } else {
                 System.out.println("Invalid Input Pls Try Again ");
             }
         }
 
-        while(true){
+        while (true) {
             System.out.print("Do You Have Other Deductibles (Social Security Contributions, etc) ? ");
             String insurance = sc.nextLine();
             if (insurance.equals("Yes")) {
                 System.out.print("Enter Amount For Other Deductibles in MMK : ");
-                deductibles+=sc.nextDouble();
+                deductibles += sc.nextDouble();
                 break;
-            }
-            else if(insurance.equals("No")){
+            } else if (insurance.equals("No")) {
                 break;
-            }
-            else {
+            } else {
                 System.out.println("Invalid Input Pls Try Again ");
             }
         }
 
-        taxableIncome=(months*monthlySalary)-(personalRelief+parentRelief+spouseRelief+childrenRelief+deductibles);
+        taxableIncome = (months * monthlySalary) - (personalRelief + parentRelief + spouseRelief + childrenRelief + deductibles);
 
-        if(taxableIncome<2000000){
-            totalTax=taxableIncome;
-        }
-        else if(taxableIncome>2000000 && taxableIncome< 5000000){
-            totalTax=(taxableIncome-2000000)*5/100;
-        }
-        else if(taxableIncome>5000000 && taxableIncome< 10000000){
-            totalTax=150000+((taxableIncome-5000000)*10/100);
-        }
-        else if(taxableIncome>10000000 && taxableIncome< 20000000){
-            totalTax=150000+500000+((taxableIncome-1000000)*15/100);
-        }
-        else if(taxableIncome>20000000 && taxableIncome< 30000000){
-            totalTax=150000+500000+1500000+((taxableIncome-1000000)*20/100);
-        }
-        else{
-            totalTax=150000+500000+1500000+200000+((taxableIncome-1000000)*25/100);
-        }
-
-        monthlyTax=totalTax/12;
         DecimalFormat df = new DecimalFormat("#,###.00");
-        System.out.println("\u001B[34m \n\n\nRESULT");
+        System.out.println("\u001B[34m \n\n\nRESULT\n");
         System.out.println("Monthly Salary                   "+df.format(monthlySalary));
         System.out.println("Annual Salary                    "+df.format(annualSalary));
         System.out.println("Income for "+months+" months             "+df.format(months*monthlySalary));
@@ -135,9 +107,81 @@ public class main {
         System.out.println("Children Relief                  "+df.format(childrenRelief));
         System.out.println("Other deductibles                "+df.format(deductibles));
         System.out.println("Taxable Income                   "+df.format(taxableIncome));
-        System.out.println("Total Tax                        "+df.format(totalTax));
+
+        int taxable;
+        int taxamount;
+        totalTax=0;
+        System.out.println("\u001B[32m\n\n\nDetails\n");
+        System.out.println("Description              Rate            Taxable           taxamount");
+        if ( taxableIncome<2000000){
+            System.out.println("Up to 20 Lakh            0%              0.00              0.00");
+            System.out.println("                                                           0.00");
+        }
+        if (taxableIncome>2000000 ){
+            taxable= (int) (taxableIncome-2000000);
+            System.out.println("Up to 20 Lakh            0%              2,000,000         0.00");
+            if(taxable>3000000){
+                System.out.println("20 Lakh - 50 Lakh        5%              3,000,000         "+df.format(150000));
+                totalTax+=150000;
+            }else {
+                taxamount= (taxable)*5/100;
+                System.out.println("20 Lakh - 50 Lakh        5%              "+df.format(taxable)+"         "+df.format(taxamount));
+                totalTax=taxamount;
+            }
+        }
+
+        if(taxableIncome>5000000) {
+            taxable = (int) (taxableIncome - 5000000);
+
+            if (taxable > 5000000) {
+                System.out.println("50 Lakh - 100 Lakh       10%             5,000,000         "+df.format(500000));
+                totalTax+=500000;
+            } else {
+                taxamount = (taxable) * 10 / 100;
+                System.out.println("50 Lakh - 100 Lakh       10%             "+df.format(taxable)+"         "+df.format(taxamount));
+
+                totalTax+=taxamount;
+            }
+        }
+
+        if(taxableIncome>10000000){
+            taxable = (int) (taxableIncome - 10000000);
+
+            if (taxable > 10000000) {
+                System.out.println("100 Lakh - 200 Lakh      15%            10,000,000         "+df.format(1500000));
+                totalTax+=1500000;
+            } else {
+                taxamount = (taxable) * 15 / 100;
+                System.out.println("100 Lakh - 200 Lakh      15%            "+df.format(taxable)+"         "+df.format(taxamount));
+                totalTax+=taxamount;
+            }
+        }
+
+        if(taxableIncome>20000000){
+            taxable = (int) (taxableIncome - 20000000);
+
+            if (taxable > 10000000) {
+                System.out.println("200 Lakh - 300 Lakh      20%            10,000,000         "+df.format(2000000));
+                totalTax+=2000000;
+                taxamount = (taxable) * 20 / 100;
+                System.out.println("Above 300 Lakh           25%            10,000,000         "+ df.format(taxamount));
+            } else {
+                taxamount = (taxable) * 20 / 100;
+                System.out.println("200 Lakh - 300 Lakh      20%            "+df.format(taxable)+"         "+df.format(taxamount));                totalTax+=taxamount;
+            }
+        }
+        System.out.println("                                                            "+df.format(totalTax));
+
+
+
+        monthlyTax=totalTax/12;
+        System.out.println("\u001B[34m\n\n\nTotal Tax                        "+df.format(totalTax));
         System.out.println("Monthly Tax                      "+df.format(monthlyTax));
 
+
     };
+    public void taxAmountOutput(double taxableIncome){
+
+    }
 
 };
